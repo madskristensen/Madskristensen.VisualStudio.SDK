333e47efe42ee2062c218d12850719daf3b46a96
Mads Kristensen <post@madskristensen.net>
2018-09-06 15:21:38 -0700

    Fixes

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
-::dotnet pack src\Microsoft.VisualStudio.SDK.14.0.csproj
-dotnet pack src\Microsoft.VisualStudio.SDK.15.0.csproj
-::dotnet pack src\Microsoft.VisualStudio.SDK.15.7.csproj
+dotnet pack --no-build --no-restore src\Microsoft.VisualStudio.SDK.14.0.csproj
+dotnet pack --no-build --no-restore src\Microsoft.VisualStudio.SDK.15.0.csproj
+dotnet pack --no-build --no-restore src\Microsoft.VisualStudio.SDK.15.7.csproj
