66519a5cdfb0e82e8415eba40362cf93cca25047
Mads Kristensen <post@madskristensen.net>
2018-09-06 15:04:38 -0700

    test

--- build.cmd
+++ build.cmd
@@ -1,3 +1,2 @@
-dotnet pack --no-build src\Microsoft.VisualStudio.SDK.14.0.csproj
-dotnet pack --no-build src\Microsoft.VisualStudio.SDK.15.0.csproj
+
 dotnet pack --no-build src\Microsoft.VisualStudio.SDK.15.7.csproj
